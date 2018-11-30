1e74a1368e2f48757b95bb22bdf87e88c9fad933
CHRISTIAN BERNARDINO CHAPARRO CAMPOS <bernar_1996@hotmail.com>
2018-11-30 16:27:16 -0700

    Initial commit

---
+++ app/src/main/java/com/example/christian/control_bluetooth/control.java
@@ -0,0 +1,13 @@
+package com.example.christian.control_bluetooth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class control extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control);
+    }
+}
